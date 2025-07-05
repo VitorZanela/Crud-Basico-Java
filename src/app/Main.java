872ae21ec8e7d2aa69bafef4321c809210a67492
vitorzanela <vitor.mardig@gmail.com>
2025-07-05 10:15:50 -0300

    Implantado opções de interação inicial do usuario

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,30 @@
 package app;
 
+import model.Layout;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Layout layout = new Layout();
+
+        //MENU PRINCIPAL
+        while (true) {
+            layout.menu(new String[]{"Cadastrar Contato","Ver Contatos", "Sair"});
+            int opc = layout.leiaInt("Escolha uma opção: ");
+            if (opc == 1) {
+                System.out.println("Estamos na opção 1");
+            } else if (opc == 2) {
+                System.out.println("Estamos na opção 2");
+            } else if (opc == 3) {
+                break;
+            } else {
+                System.out.println("Opção Incorreta. Tente novamente!!");
+            }
+        }
+
+
+
+
+    }
 }
